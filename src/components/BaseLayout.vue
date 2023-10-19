6598dc3c80c30bac0ffb89647fa6b70dd382082a
Anıl Kaynar <aanilkay@gmail.com>
2023-10-20 00:36:30 +0300

    Empty Link options is added. And styling improvements.

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -1,18 +1,23 @@
 <template>
     
-  components: { PhotoCards },
-  components: { PhotoCards },
-  components: { PhotoCards },<v-layout class="rounded rounded-md">
-    <v-app-bar title="Application bar"></v-app-bar>
-
-    <v-navigation-drawer>
+  <v-layout class="rounded rounded-md">
+    <v-navigation-drawer
+         expand-on-hover
+         rail
+         location="left"
+    >
       <v-list>
         <v-list-item title="Navigation drawer"></v-list-item>
+        <v-list-item title="Navigation drawer"></v-list-item>
+        <v-list-item title="Navigation drawer"></v-list-item>
+
       </v-list>
     </v-navigation-drawer>
 
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px;" v-if="dataIsReady">
-        <v-row class="full-height-row">
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;padding: 4%">
+      
+
+        <v-row>
             
             <photo-cards  v-for="(photo,index) in photoDatas" :key="index" :src="photo.src" :prompt="photo.prompt"> </photo-cards>
 
@@ -30,7 +35,6 @@ import photoData from "../assets/photoData.json";
 // Reaktif veri tanımlamaları
 const photos = ref(photoData.photos);
 const photoDatas = ref([]);
-const dataIsReady = ref(false);
 const prompt = ref("aaa");
 
 // Fotoğrafları al
@@ -48,18 +52,11 @@ const fetchPhotos = () => {
 };
 
 // Prompt'u al
-const fetchPrompt = () => {
-  axios.get("/contents/a_man_with_a_gun_in_his_left_hand_and_a_rose_in_his_right_hand__S789114546_St50_G7.5.json")
-    .then((data) => {
-      prompt.value = data.data.prompt;
-      dataIsReady.value = true;
-    });
-};
+
 
 // Component mount edildiğinde data fetch işlemleri başlatılır
 onMounted(() => {
   fetchPhotos();
-  fetchPrompt();
 });
 
 </script>
--- src/components/PhotoCards.vue
+++ src/components/PhotoCards.vue
@@ -1,17 +1,23 @@
 <template>
     <v-card
     class="mx-auto"
-    max-width="344"
+    max-width="512"
   >
     <v-img
       :src="src"
-      height="200px"
       cover
     ></v-img>
 
-    <v-card-title>
+    <v-card-title class="wrap-text">
         {{prompt}}
-    </v-card-title>
+    </v-card-title >
+     <v-btn
+      :href="linkJson"
+      color="primary"
+      text="JsonLink"
+      dark
+    >
+     </v-btn>
     </v-card>
 </template>
 
@@ -28,7 +34,17 @@ export default {
         required:false,
          default:"Top western road trips"
 
+    },
+    linkJson:{
+      type:String,
+      required:false,
+      default:"/contents/Medium_Shot_Photo_of_a_Programmer_writing_code_in_a_Coffee_Shop_with_a_S4248866250_St50_G7.5.json"
     }
   }
 }
 </script>
+<style scoped>
+.wrap-text {
+  white-space: normal !important; /* normal, metnin satırlara bölünmesini sağlar */
+}
+</style>
