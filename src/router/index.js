b22f2d64cf07fc357475532c41fd7dde57cfe6ea
Anıl Kaynar <aanilkay@gmail.com>
2023-10-23 22:23:56 +0300

    Vueform example router is change.

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,12 @@ const routes = [
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
-        path: 'test',
-        name: 'test',
+        path: 'vueform',
+        name: 'vueform',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: () => import(/* webpackChunkName: "home" */ '@/views/VueFormExample.vue'),
       },
       {
         path:"",
